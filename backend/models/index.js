13158168cf8228e377ece3c231695ac3f6b5df86
siyaredkar29 <150668074+siyaredkar29@users.noreply.github.com>
2025-09-19 19:58:34 +0530

    config changes

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 'use strict';
 
 const fs = require('fs');
@@ -6,7 +7,8 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
+
 const db = {};
 
 let sequelize;
